515ff8a23fcf47579d3904ff20851e004e4f4f18
tropchan <tropchan@alumni.uwo.ca>
2015-05-28 01:08:19 -0400

    Implimented deivse for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for heroku
+  #note to set this to your actual host domain
+  config.action_mailer.default_url_options = { host: '', port: 3000 }
 end
